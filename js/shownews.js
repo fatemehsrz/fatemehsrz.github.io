06b9862deca07837ff9cabe658efc66fa97f0f18
Fatima <fatemehsrz4@gmail.com>
2024-10-28 16:03:54 +0100

    update

--- js/shownews.js
+++
@@ -1,6 +0,0 @@
-function showNews() {
-    var x = document.getElementById("hidden_news");
-    x.style.display = "block";
-    var x = document.getElementById("hide_news_botton");
-    x.style.display = "none";
-}
